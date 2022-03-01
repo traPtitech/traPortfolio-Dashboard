5188df1643974b14fac437a294a2b2062deed69f
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-03-01 21:42:48 +0900

    routerを更新

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const Index = () => import('/@/pages/Index.vue')
-const Search = () => import('/@/pages/Search.vue')
 const User = () => import('/@/pages/User.vue')
-const Groups = () => import('/@/pages/Groups.vue')
-const Group = () => import('/@/pages/Group.vue')
 const Projects = () => import('/@/pages/Projects.vue')
 const Project = () => import('/@/pages/Project.vue')
 const Events = () => import('/@/pages/Events.vue')
 const Event = () => import('/@/pages/Event.vue')
 const Contests = () => import('/@/pages/Contests.vue')
 const Contest = () => import('/@/pages/Contest.vue')
-const ContestTeam = () => import('/@/pages/ContestTeam.vue')
+const ContestCreate = () => import('/@/pages/ContestCreate.vue')
 
 const routes = [
   {
@@ -20,36 +17,12 @@ const routes = [
     component: Index
   },
   {
-    path: '/search',
-    name: 'UserSearch',
-    component: Search
-  },
-  {
-    path: '/users',
-    redirect: '/search'
-  },
-  {
-    path: '/users/:userId',
-    name: 'User',
-    component: User
-  },
-  {
-    path: '/groups',
-    name: 'Groups',
-    component: Groups
-  },
-  {
-    path: '/groups/:groupId',
-    name: 'Group',
-    component: Group
-  },
-  {
     path: '/projects',
     name: 'Projects',
     component: Projects
   },
   {
-    path: '/projects/:projectId',
+    path: '/projects/:id',
     name: 'Project',
     component: Project
   },
@@ -59,7 +32,7 @@ const routes = [
     component: Events
   },
   {
-    path: '/events/:eventId',
+    path: '/events/:id',
     name: 'Event',
     component: Event
   },
@@ -69,14 +42,19 @@ const routes = [
     component: Contests
   },
   {
-    path: '/contests/:contestId',
+    path: '/contests/:id',
     name: 'Contest',
     component: Contest
   },
   {
-    path: '/contests/:contestId/:teamId',
-    name: 'ContestTeam',
-    component: ContestTeam
+    path: '/contests/create',
+    name: 'ContestCreate',
+    component: ContestCreate
+  },
+  {
+    path: '/user',
+    name: 'User',
+    component: User
   }
 ]
 
