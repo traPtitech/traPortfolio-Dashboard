6fbccf366576b4a2be8e9a17bb49e3bd91a1c6bf
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-03-01 22:20:17 +0900

    サイドバーを仮置き

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <page-header />
-  <Suspense>
-    <template #default>
-      <router-view :class="$style.main" />
-    </template>
-    <template #fallback>
-      <loading />
-    </template>
-  </Suspense>
-  <page-footer />
+  <div :class="$style.container">
+    <page-sidebar />
+    <Suspense>
+      <template #default>
+        <router-view :class="$style.main" />
+      </template>
+      <template #fallback>
+        <loading />
+      </template>
+    </Suspense>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import PageHeader from '/@/components/Layout/PageHeader.vue'
+import PageSidebar from '/@/components/Layout/PageSidebar.vue'
 import Loading from '/@/pages/Loading.vue'
-import PageFooter from '/@/components/Layout/PageFooter.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
     PageHeader,
     Loading,
-    PageFooter
+    PageSidebar
   }
 })
 </script>
@@ -31,5 +33,10 @@ export default defineComponent({
 .main {
   flex: 1;
   padding-bottom: 1rem;
+}
+.container {
+  display: flex;
+  align-items: stretch;
+  height: calc(100vh - 5rem);
 }
 </style>
---
+++ src/components/Layout/PageSidebar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :class="$style.container">
+    <div>Top</div>
+    <div>Top</div>
+    <div>Top</div>
+    <div>Top</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageSidebar'
+})
+</script>
+
+<style lang="scss" module>
+.container {
+  background-color: $color-secondary;
+  width: 260px;
+}
+</style>
