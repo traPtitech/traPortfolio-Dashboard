30ad6568b0620e02f4273e2a4aee3a945aaf7d66
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-05-13 19:13:33 +0900

    objectsyncerを追加

--- src/use/vModel.ts
+++ src/use/vModel.ts
@@ -31,3 +31,30 @@ export const useModelValueSyncer = <
 ): WritableComputedRef<P['modelValue']> => {
   return useModelSyncer(props, emit, 'modelValue', onUpdate)
 }
+
+type ToSyncedRefs<T> = { [KK in keyof T]: WritableComputedRef<T[KK]> }
+
+export const useModelObjectSyncer = <
+  P,
+  K extends keyof P & string,
+  E extends (n: `update:${K}`, val: P[K]) => void
+>(
+  props: P,
+  emit: E,
+  key = 'modelValue' as K
+): ToSyncedRefs<P[K]> => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const computedObject: ToSyncedRefs<P[K]> = {} as any
+
+  for (const k of Object.keys(props[key]) as Array<keyof P[K]>) {
+    computedObject[k] = computed({
+      get() {
+        return props[key][k]
+      },
+      set(v) {
+        emit(`update:${key}` as const, { ...props[key], [k]: v })
+      }
+    })
+  }
+  return computedObject
+}
