d0ce65b895aed66e7c8f8f6d0b88072775dd90c4
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-10-06 01:53:10 +0900

    🚧 ⚡ MemberInputでusericonの表示と無限スクロールを実装

---
+++ src/components/UI/MemberInput.vue
@@ -0,0 +1,101 @@
+<script lang="ts" setup>
+import vSelect from 'vue-select'
+import 'vue-select/dist/vue-select.css'
+
+import { computed, nextTick, ref, watchEffect } from 'vue'
+import apis, { User } from '/@/lib/apis'
+import UserIcon from './UserIcon.vue'
+
+interface Props {
+  modelValue: User[]
+}
+
+const props = defineProps<Props>()
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: User[]): void
+}>()
+
+const value = computed({
+  get: () => props.modelValue,
+  set: v => emit('update:modelValue', v)
+})
+
+const limit = ref(10)
+const search = ref('')
+const users = ref<User[]>([])
+watchEffect(async () => {
+  const res = await apis.getUsers()
+  if (res.status === 200) {
+    users.value = res.data
+  }
+})
+const filtered = computed(() =>
+  users.value.filter(user => user.name.includes(search.value))
+)
+const options = computed(() => filtered.value.slice(0, limit.value))
+const hasNextPage = computed(() => filtered.value.length > options.value.length)
+
+const onSearch = (v: string) => {
+  search.value = v
+}
+
+const infiniteScroll = async (entries: IntersectionObserverEntry[]) => {
+  const entry = entries[0]
+  if (entry !== undefined && entry.isIntersecting) {
+    const ul = (entry.target as HTMLUListElement).offsetParent
+    const scrollTop = ul?.scrollTop ?? 0
+    limit.value += 5
+
+    await nextTick()
+    if (ul !== null) {
+      ul.scrollTop = scrollTop
+    }
+  }
+}
+
+const footerRef = ref<HTMLUListElement>()
+const observer = new IntersectionObserver(infiniteScroll)
+const onOpen = async () => {
+  if (!hasNextPage.value) return
+
+  await nextTick()
+  if (footerRef.value !== undefined) {
+    observer.observe(footerRef.value)
+  }
+}
+const onClose = () => {
+  observer.disconnect()
+}
+</script>
+
+<template>
+  <v-select
+    v-model="value"
+    :options="options"
+    label="name"
+    multiple
+    @open="onOpen"
+    @close="onClose"
+    @search="onSearch"
+  >
+    <template #option="{ name }">
+      <li :class="$style.items">
+        <user-icon :name="name" />
+        <p>{{ name }}</p>
+      </li>
+    </template>
+    <template #list-footer>
+      <li v-if="hasNextPage" ref="footerRef">Loading...</li>
+    </template>
+  </v-select>
+</template>
+
+<style lang="scss" module>
+.items {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  height: 2rem;
+}
+</style>
---
+++ src/components/UI/UserIcon.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+interface Props {
+  name: string
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <img
+    alt="icon"
+    :class="$style.icon"
+    :src="'https://q.trap.jp/api/v3/public/icon/toshi00'"
+  />
+</template>
+
+<style lang="scss" module>
+.icon {
+  border-radius: 50%;
+  height: 100%;
+}
+</style>
