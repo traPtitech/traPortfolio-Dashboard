50ed0c78f237bb7f7393342ba80df87ba6f9eae9
mehm8128 <mehm8128@gmail.com>
2022-10-15 20:59:49 +0900

    remove mock

--- src/pages/UserAccounts.vue
+++ src/pages/UserAccounts.vue
@@ -3,32 +3,18 @@ import ContentHeader from '/@/components/Layout/ContentHeader.vue'
 import PageContainer from '/@/components/Layout/PageContainer.vue'
 import BaseButton from '/@/components/UI/BaseButton.vue'
 
-import type { Account } from '/@/lib/apis'
 import AccountItem from '/@/components/UserAccounts/AccountItem.vue'
+import { ref } from 'vue'
+import apis from '/@/lib/apis'
+import useUserDataFetcher from '/@/use/userDataFetcher'
 
-const accounts: Account[] = [
-  {
-    id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-    displayName: 'mehm8128',
-    type: 5,
-    prPermitted: true,
-    url: 'https://github.com/mehm8128'
-  },
-  {
-    id: '3fa85f64-5717-4562-b3fc-2c963f66afa7',
-    displayName: 'mehm8128',
-    type: 8,
-    prPermitted: false,
-    url: 'https://atcoder.jp/users/mehm8128'
-  },
-  {
-    id: '3fa85f64-5717-4562-b3fc-2c963f66afa8',
-    displayName: 'mehm8128',
-    type: 1,
-    prPermitted: true,
-    url: 'https://trap.jp/author/mehm8128/'
-  }
-]
+const userId = ref('c714a848-2886-4c10-a313-de9bc61cb2bb')
+// todo: get meが実装されたらそれを使う
+
+const { data: accounts, fetcherState: fetcherState } = useUserDataFetcher(
+  userId,
+  userId => apis.getUserAccounts(userId)
+)
 </script>
 
 <template>
@@ -47,11 +33,13 @@ const accounts: Account[] = [
         <BaseButton type="primary" icon="mdi:account">New</BaseButton>
       </router-link>
     </div>
-    <ul :class="$style.accountList">
+    <ul v-if="fetcherState === 'loaded'" :class="$style.accountList">
       <li v-for="account in accounts" :key="account.id">
         <account-item :account="account" />
       </li>
     </ul>
+    <p v-else-if="fetcherState === 'loading'">ローディング中...</p>
+    <p v-else-if="fetcherState === 'error'">エラーが発生しました</p>
 
     <router-link to="/user" :class="$style.link">
       <BaseButton
