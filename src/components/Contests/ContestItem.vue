2fbf1b12a30beb42a90dbe3baebeb81ad4642c9c
mehm8128 <mehm8128@gmail.com>
2022-10-25 23:14:41 +0900

    make contests page

---
+++ src/components/Contests/ContestItem.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+import type { ContestTeamWithContestName } from '/@/lib/apis'
+import Icon from '/@/components/UI/Icon.vue'
+
+interface Props {
+  contest: ContestTeamWithContestName
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <router-link :to="`/contests/${contest.id}/edit`" :class="$style.link">
+    <div :class="$style.container">
+      <p :class="$style.displayName">{{ contest.name }}</p>
+      <p :class="$style.duration"><icon name="mdi:calendar" />2020/12/30</p>
+      <p :class="$style.teams"><icon name="mdi:account-group" />23チーム</p>
+    </div>
+  </router-link>
+</template>
+
+<style lang="scss" module>
+.link {
+  color: inherit;
+  text-decoration: none;
+}
+.container {
+  padding: 0.5rem;
+}
+
+.displayName {
+  color: $color-primary;
+  font-size: 1.125rem;
+}
+.duration {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  margin-top: 0.75rem;
+}
+.teams {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  margin-top: 0.5rem;
+}
+</style>
--- src/pages/Contests.vue
+++ src/pages/Contests.vue
@@ -1,15 +1,83 @@
+<script lang="ts" setup>
+import ContentHeader from '/@/components/Layout/ContentHeader.vue'
+import PageContainer from '/@/components/Layout/PageContainer.vue'
+import BaseButton from '/@/components/UI/BaseButton.vue'
+import ContestItem from '/@/components/Contests/ContestItem.vue'
+
+import { ref } from 'vue'
+import apis from '/@/lib/apis'
+import useUserDataFetcher from '/@/use/userDataFetcher'
+import FormInput from '/@/components/UI/FormInput.vue'
+
+const userId = ref('c714a848-2886-4c10-a313-de9bc61cb2bb')
+// todo: get meが実装されたらそれを使う
+
+const { data: contests, fetcherState } = useUserDataFetcher(userId, userId =>
+  apis.getUserContests(userId)
+)
+const searchQuery = ref('')
+</script>
+
 <template>
-  <div>Contests</div>
+  <page-container>
+    <content-header
+      icon-name="mdi:trophy"
+      :header-texts="[{ title: 'Contests', url: '/contests' }]"
+      detail="コンテスト情報を変更します。"
+      :class="$style.header"
+    />
+    <div :class="$style.searchFormContainer">
+      <FormInput
+        v-model="searchQuery"
+        :limit="5"
+        placeholder="コンテスト名"
+        icon="magnify"
+        :class="$style.searchForm"
+      />
+      <router-link to="/contests/new" :class="$style.link">
+        <BaseButton type="primary" icon="mdi:plus">New</BaseButton>
+      </router-link>
+    </div>
+    <ul v-if="fetcherState === 'loaded'" :class="$style.contestList">
+      <li v-for="contest in contests" :key="contest.id">
+        <contest-item :contest="contest" />
+      </li>
+    </ul>
+    <p v-else-if="fetcherState === 'loading'">ローディング中...</p>
+    <p v-else-if="fetcherState === 'error'">エラーが発生しました</p>
+  </page-container>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'Contests',
-  components: {},
-  setup() {
-    return {}
+<style lang="scss" module>
+.searchFormContainer {
+  display: flex;
+  align-items: center;
+  margin-bottom: 1rem;
+}
+.header {
+  margin: 4rem 0 2rem;
+}
+.searchForm {
+  flex-grow: 1;
+}
+.link {
+  text-decoration: none;
+  color: inherit;
+  margin-left: 0.5rem;
+}
+.contestList {
+  list-style: none;
+  padding: 0.5rem 0;
+  li {
+    border: 1px solid $color-primary-text;
+    border-radius: 8px;
+    margin-bottom: 0.5rem;
+    &:last-child {
+      margin-bottom: 0;
+    }
+    &:hover {
+      background-color: $color-background-dim;
+    }
   }
-})
-</script>
+}
+</style>
