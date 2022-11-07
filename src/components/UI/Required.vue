922d5853cfbd873ef3afb94bab18b243b0cf002a
tesso57 <tesso1729@gmail.com>
2022-11-07 13:44:48 +0900

    ✨ 必須タグの追加

---
+++ src/components/UI/Required.vue
@@ -0,0 +1,16 @@
+<template>
+  <div :class="$style.required">必須</div>
+</template>
+
+<style lang="scss" module>
+.required {
+  display: inline-block;
+
+  border-radius: 8px;
+  padding: 0 0.5rem;
+  background-color: $color-warning;
+
+  color: $color-primary-text;
+  font-size: 0.75rem;
+}
+</style>
