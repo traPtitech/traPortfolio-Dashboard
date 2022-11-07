b1504d559eb693f4c379f5f9283a472f28713270
tesso <48358052+tesso57@users.noreply.github.com>
2022-11-07 20:15:20 +0900

    🚧 TODO: Contestsのmerge

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,15 +1,55 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import ContentHeader from '../components/Layout/ContentHeader.vue'
+import PageContainer from '../components/Layout/PageContainer.vue'
+import BaseButton from '../components/UI/BaseButton.vue'
+import FormInput from '../components/UI/FormInput.vue'
+
+const searchQuery = ref('')
+</script>
+
 <template>
-  <div>Projects</div>
+  <page-container>
+    <content-header
+      icon-name="mdi:clipboard-file-outline"
+      :header-texts="[{ title: 'Projects', url: '/' }]"
+      detail="プロジェクトを変更します"
+      :class="$style.header"
+    />
+    <div :class="$style.searchFormContainer">
+      <div :class="$style.searchForm">
+        <p :class="$style.body2">検索</p>
+        <form-input
+          v-model="searchQuery"
+          placeholder="プロジェクト名"
+          icon="magnify"
+        />
+      </div>
+      <div>
+        <p :class="$style.body2">プロジェクト作成</p>
+        <!-- TODO: Contestsの作成が終わったら、それをマージ。 -->
+        <base-button type="primary" icon="mdi:clipboard-file"> New</base-button>
+      </div>
+    </div>
+  </page-container>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<style lang="scss" module>
+.header {
+  margin: 4rem 0 2rem;
+}
 
-export default defineComponent({
-  name: 'Projects',
-  components: {},
-  setup() {
-    return {}
-  }
-})
-</script>
+.searchFormContainer {
+  display: flex;
+  gap: 0.5rem;
+}
+
+.searchForm {
+  flex-grow: 1;
+}
+
+.body2 {
+  font-size: 0.875rem;
+  color: $color-secondary;
+}
+</style>
