253205432c28f8e7ada22b9012c199f7ee86da14
mehm8128 <mehm8128@gmail.com>
2022-12-30 22:32:15 +0900

    add userIcons

--- src/components/Contest/ContestTeamItem.vue
+++ src/components/Contest/ContestTeamItem.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import UserIcons from '/@/components/UI/UserIcons.vue'
 import type { ContestTeam } from '/@/lib/apis'
 
 interface Props {
@@ -7,13 +8,17 @@ interface Props {
 }
 
 defineProps<Props>()
+const userIds = ['sapphi_red', 'toshi00', 'tesso', 'mehm8128']
 </script>
 
 <template>
   <router-link :to="`/contests/${contestTeam.id}`" :class="$style.link">
     <div :class="$style.container">
-      <p :class="$style.name">{{ contestTeam.name }}</p>
-      <p :class="$style.result">{{ contestTeam.result }}</p>
+      <div>
+        <p :class="$style.name">{{ contestTeam.name }}</p>
+        <p :class="$style.result">{{ contestTeam.result }}</p>
+      </div>
+      <UserIcons :user-ids="userIds" />
     </div>
   </router-link>
 </template>
@@ -25,9 +30,15 @@ defineProps<Props>()
 }
 .container {
   padding: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 .name {
   color: $color-primary;
   font-size: 1.125rem;
+}
+.result {
+  margin-top: 2rem;
 }
 </style>
---
+++ src/components/UI/UserIcons.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import UserIcon from '/@/components/UI/UserIcon.vue'
+interface Props {
+  userIds: string[]
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div :class="$style.userIcons">
+    <UserIcon
+      v-for="(userId, i) in userIds.slice(0, 3)"
+      :key="userId"
+      :user-id="userId"
+      :class="$style.userIcon"
+      :style="{ left: `${i * 16}px` }"
+    />
+    <span>+{{ userIds.length }}</span>
+  </div>
+</template>
+
+<style lang="scss" module>
+.userIcons {
+  width: 80px;
+  position: relative;
+  text-align: right;
+}
+.userIcon {
+  position: absolute;
+}
+</style>
