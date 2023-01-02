8fb41a2c99ec8530b2021d8106d847e1b74accda
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2023-01-02 20:03:30 +0900

    👕 lint

--- src/components/UI/BaseSelect.vue
+++ src/components/UI/BaseSelect.vue
@@ -5,62 +5,75 @@ import { computed } from 'vue'
 import Icon from '/@/components/UI/Icon.vue'
 
 interface Props {
-    modelValue: string
-    options: string[]
-    placeholder?: string
+  modelValue: string
+  options: string[]
+  placeholder?: string
 }
 
-const props = withDefaults(defineProps<Props>(),
-    {
-        placeholder: ''
-    })
+const props = withDefaults(defineProps<Props>(), {
+  placeholder: ''
+})
 
 const emit = defineEmits<{
-    (e: 'update:modelValue', value: string): void
+  (e: 'update:modelValue', value: string): void
 }>()
 
 const value = computed({
-    get: () => props.modelValue,
-    set: v => emit('update:modelValue', v)
+  get: () => props.modelValue,
+  set: v => emit('update:modelValue', v)
 })
-
 </script>
 
 <template>
-    <v-select v-model="value" :options="options" :clearable="false" :class="$style.select">
-        <template #option="{ label }">
-            <div :class="$style.item">
-                <icon name="mdi:tick-circle-outline" :class="$style.icon" />
-                <p>{{ label }}</p>
-            </div>
-        </template>
-    </v-select>
+  <v-select
+    v-model="value"
+    :options="options"
+    :clearable="false"
+    :class="$style.select"
+  >
+    <template #option="{ label }">
+      <div :class="$style.item">
+        <icon
+          v-if="value === label"
+          name="mdi:tick-circle-outline"
+          :class="$style.icon"
+        />
+        <p>{{ label }}</p>
+      </div>
+    </template>
+  </v-select>
 </template>
 
 <style lang="scss" module>
+.select {
+  --vs-controls-color: #{$color-secondary-text};
+}
 .select :global(.vs__dropdown-toggle) {
-    border-color: $color-secondary;
-    border-bottom-left-radius: 4px;
-    border-bottom-right-radius: 4px;
+  border-color: $color-secondary;
+  border-bottom-left-radius: 4px;
+  border-bottom-right-radius: 4px;
+}
+
+.select :global(.vs__actions) {
+  color: $color-secondary-text;
 }
 
 .select:global(.vs--open .vs__dropdown-toggle) {
-    border-color: $color-primary;
+  border-color: $color-primary;
 }
 
-.select :global(.vs__dropdown-option)[aria-selected="false"] .icon {
-    visibility: hidden;
+.icon {
+  color: $color-primary;
 }
-
 
 .select :global(.vs__dropdown-menu) {
-    margin-top: 4px;
-    border-radius: 4px;
-    border-color: $color-primary;
+  margin-top: 4px;
+  border-radius: 4px;
+  border-color: $color-primary;
 }
 
 .item {
-    display: flex;
-    gap: 0.5rem;
+  display: flex;
+  gap: 0.5rem;
 }
 </style>
