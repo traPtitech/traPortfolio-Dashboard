52a06c81a7d89a971a32b82fd7a114976571a96d
tesso <48358052+tesso57@users.noreply.github.com>
2023-01-03 23:14:32 +0900

    🚧 原型の完成

---
+++ src/components/UI/ComfirmModal.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const modalRef = ref<HTMLDialogElement>()
+
+const open = () => {
+  modalRef.value?.showModal()
+  modalRef.value?.addEventListener('click', listener)
+}
+
+const close = () => {
+  modalRef.value?.close()
+  modalRef.value?.removeEventListener('click', listener)
+}
+
+const listener = (e: Event) => {
+  if (modalRef.value === undefined) return
+  if (e.target === modalRef.value) {
+    close()
+  }
+}
+</script>
+
+<template>
+  <button @click="open">おせ</button>
+  <dialog ref="modalRef" :class="$style.content">
+    <div id="#dialog-container">
+      <p>Hello World</p>
+      <button @click="close">Close Modal</button>
+    </div>
+  </dialog>
+</template>
+
+<style lang="scss" module>
+.content {
+  margin: auto;
+}
+</style>
