3a8762cc1dbeb4f85108770b9e04a114199ad7aa
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-11-05 00:00:20 +0900

    📚 deepFreeze に JSDoc 書く

--- src/lib/deepFreeze.ts
+++ src/lib/deepFreeze.ts
@@ -1,3 +1,10 @@
+/**
+ * オブジェクトや配列の全てのプロパティを再帰的にfreezeする
+ *
+ * 自己参照など閉路が存在する場合は無限ループに陥るので注意
+ *
+ * @see https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze
+ */
 export const deepFreeze = <const T extends object>(obj: T): T => {
   const propNames = Object.getOwnPropertyNames(obj)
 
