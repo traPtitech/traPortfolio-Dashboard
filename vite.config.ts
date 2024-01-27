3df2aa1c3c845f27a90312fcdf0ac2ab082549ed
mehm8128 <mehm8128@gmail.com>
2024-01-27 13:22:39 +0900

    set base

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import PurgeIcons from 'vite-plugin-purge-icons'
 const srcPath = path.resolve(__dirname, 'src').replace(/\\/g, '/')
 
 const config: UserConfig = {
+  base: './',
   resolve: {
     alias: {
       '/@': srcPath
