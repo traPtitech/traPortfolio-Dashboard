d5771ba1ba7faa0e68073b76a7a4a9ac18afee33
mehm8128 <mehm8128@gmail.com>
2024-05-12 23:41:14 +0900

    format

--- src/components/UI/Icon.vue
+++ src/components/UI/Icon.vue
@@ -9,12 +9,7 @@ withDefaults(defineProps<Props>(), { size: 24 })
 </script>
 
 <template>
-  <icon
-    :icon="name"
-    :height="size"
-    :width="size"
-    :class="$style.icon"
-  />
+  <icon :icon="name" :height="size" :width="size" :class="$style.icon" />
 </template>
 
 <style lang="scss" module>
