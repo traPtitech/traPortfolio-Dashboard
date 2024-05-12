8c68df1f17f5eff9d587e8002d26b5ecd7e39cf9
Pugma <maronmarondelicious@gmail.com>
2024-05-12 16:13:28 +0900

    CSSの削除と統合

--- src/components/Layout/PageHeader.vue
+++ src/components/Layout/PageHeader.vue
@@ -17,19 +17,17 @@ const { isMobile } = storeToRefs(useResponsiveStore())
 
 <template>
   <div :class="$style.container">
-    <div :class="$style.leftContainer">
-      <button v-if="isMobile" @click="emit('toggleNavigationBar')">
-        <icon name="mdi:menu" />
-      </button>
-      <router-link to="/">
-        <img
-          src="/@/assets/traP_logo_blue.svg"
-          alt="traP"
-          width="343"
-          height="48"
-        />
-      </router-link>
-    </div>
+    <button v-if="isMobile" @click="emit('toggleNavigationBar')">
+      <icon name="mdi:menu" />
+    </button>
+    <router-link to="/">
+      <img
+        src="/@/assets/traP_logo_blue.svg"
+        alt="traP"
+        width="343"
+        height="48"
+      />
+    </router-link>
   </div>
 </template>
 
@@ -41,26 +39,7 @@ const { isMobile } = storeToRefs(useResponsiveStore())
   justify-content: space-between;
   align-items: center;
   border-bottom: solid 0.1rem $color-secondary;
-}
-
-.leftContainer {
-  display: flex;
-  align-items: center;
   gap: 1rem;
-}
-
-.rightContainer {
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-}
-
-.syncButton {
-  color: $color-secondary;
-  transition: 0.2s all ease-in-out;
-  &:hover {
-    color: $color-primary;
-  }
 }
 
 @media (width <= 768px) {
