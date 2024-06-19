bc6ef209492139b45ba1f46415ac20ccfc324665
Pugma <maronmarondelicious@gmail.com>
2024-06-19 11:35:16 +0900

    GHAで動作するnodeのバージョンを更新

--- scripts/generateApi.js
+++ scripts/generateApi.js
@@ -28,7 +28,6 @@ const generateCmd = [
 if (process.env.SKIP_GENAPI) {
   // eslint-disable-next-line no-console
   console.log('Skipped generating apis.')
-  process.exitCode = 0
 }
 
 ;(async () => {
