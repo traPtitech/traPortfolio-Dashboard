8372a2642fa3f7ba918628583e7e9fa578a5640c
ogu-kazemiya <135459827+ogu-kazemiya@users.noreply.github.com>
2024-08-15 02:26:54 +0900

    UserIconにalt, width, heightをつけた

--- src/components/UI/UserIcon.vue
+++ src/components/UI/UserIcon.vue
@@ -16,7 +16,14 @@ const styles = computed(() => ({
 </script>
 
 <template>
-  <img :class="$style.icon" :src="src" :style="styles" />
+  <img
+    :class="$style.icon"
+    :src="src"
+    :style="styles"
+    :width="size"
+    :height="size"
+    :alt="userName"
+  />
 </template>
 
 <style lang="scss" module>
