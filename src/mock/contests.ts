29d83d232858bcc6d18309dc909f48ef3a44de9a
Futadaruma <0098yuta@gmail.com>
2025-01-20 20:01:14 +0900

    contestsのモックを実装

--- src/mock/contests.ts
+++ src/mock/contests.ts
@@ -1,1 +1,41 @@
-export const handlers = []
+import { http, HttpResponse } from 'msw'
+import { Contest, ContestDetail } from '../lib/apis'
+
+export const handlers = [
+  // apis.getContests
+  http.get<never, never, Contest[], '/api/v1/contests'>(
+    '/api/v1/contests',
+    () => {
+      return HttpResponse.json([
+        {
+          id: 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
+          name: 'sampleUser',
+          duration: {
+            since: '2021-01-01T00:00:00Z',
+            until: '2021-01-01T00:00:00Z'
+          }
+        }
+      ])
+    }
+  ),
+
+  // apis.getContest
+  http.get<
+    { contestId: string },
+    never,
+    ContestDetail,
+    '/api/v1/contests/:contestId'
+  >('/api/v1/contests/:contestId', () => {
+    return HttpResponse.json({
+      id: 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
+      name: 'sampleUser',
+      description: 'sampleUser',
+      duration: {
+        since: '2021-01-01T00:00:00Z',
+        until: '2021-01-01T00:00:00Z'
+      },
+      link: '',
+      teams: []
+    })
+  })
+]
