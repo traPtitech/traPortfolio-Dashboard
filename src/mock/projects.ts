5d269891bbdf8733d79859c84b557eab9793772b
Futadaruma <0098yuta@gmail.com>
2025-01-21 18:08:29 +0900

    projectsのモックの枠組みを実装

--- src/mock/projects.ts
+++ src/mock/projects.ts
@@ -1,1 +1,41 @@
-export const handlers = []
+import { http, HttpResponse } from 'msw'
+import { Project, ProjectDetail } from '../lib/apis'
+
+export const handlers = [
+  // apis.getProjects
+  http.get<never, never, Project[], '/api/v1/projects'>(
+    '/api/v1/projects',
+    () => {
+      return HttpResponse.json([
+        {
+          id: 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
+          name: 'sampleProject',
+          duration: {
+            since: { year: 2021, semester: 1 },
+            until: { year: 2021, semester: 1 }
+          }
+        }
+      ])
+    }
+  ),
+
+  // apis.getProject
+  http.get<
+    { contestId: string },
+    never,
+    ProjectDetail,
+    '/api/v1/projects/:projectsId'
+  >('/api/v1/projects/:projectsId', () => {
+    return HttpResponse.json({
+      id: 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
+      name: 'sampleProject',
+      description: 'sampleProject',
+      duration: {
+        since: { year: 2021, semester: 1 },
+        until: { year: 2021, semester: 1 }
+      },
+      link: '',
+      members: []
+    })
+  })
+]
