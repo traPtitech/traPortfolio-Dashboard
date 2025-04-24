e831bbebb80f91232eb2b2674ce5f9f035d0cb98
Futadaruma <0098yuta@gmail.com>
2025-04-24 15:24:57 +0900

    deleted the unnecessary function

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -3,10 +3,6 @@ import { handlers as contestHandlers } from './contests'
 import { handlers as groupeHandlers } from './groups'
 import { handlers as projectHandlers } from './projects'
 
-export const makeRelativeUrl = (url: string) => {
-  return `/api/v1${url}`
-}
-
 export const handlers = [
   ...userHandlers,
   ...contestHandlers,
