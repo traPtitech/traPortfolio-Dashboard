2e7215b56ae142f58edcf26aeb63cedca53f318a
Futadaruma <0098yuta@gmail.com>
2025-06-27 09:45:22 +0900

    added sample group data

--- src/mocks/groups.ts
+++ src/mocks/groups.ts
@@ -1,46 +1,51 @@
 import { http, HttpResponse } from 'msw'
-import { Group, GroupDetail, User } from '/@/lib/apis'
+import { Group, GroupDetail } from '../lib/apis'
+import { sampleUsers } from './users'
+
+const sampleGroupDetail: GroupDetail = {
+  id: 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
+  name: 'サンプル班1 -sample group 1-',
+  link: 'https://example.com',
+  description: 'Sample description for group 1',
+  admin: sampleUsers,
+  members: sampleUsers.map(user => {
+    return {
+      id: user.id,
+      name: user.name,
+      realName: user.realName,
+      description: '',
+      duration: {
+        since: { year: 2015, semester: 0 },
+        until: { year: 2021, semester: 1 }
+      }
+    }
+  })
+}
+
+const sampleGroup: Group = {
+  id: sampleGroupDetail.id,
+  name: sampleGroupDetail.name
+}
 
 export const handlers = [
   // apis.getProjects
   http.get<never, never, Group[], '/api/v1/groups'>('/api/v1/groups', () => {
-    return HttpResponse.json([
-      {
-        id: 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
-        name: 'sampleGroup'
-      }
-    ])
+    return HttpResponse.json(Array(10).fill(sampleGroup))
   }),
 
   // apis.getProject
   http.get<{ groupId: string }, never, GroupDetail, '/api/v1/groups/:groupId'>(
     '/api/v1/groups/:groupId',
     () => {
-      return HttpResponse.json({
-        id: 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
-        name: 'sampleGroup',
-        description: 'sampleGroup',
-        link: '',
-        admin: [],
-        members: []
-      })
+      return HttpResponse.json(sampleGroupDetail)
     }
   ),
 
   // apis.getUserGroups
-  http.get<{ userId: string }, never, User[], '/api/v1/users/:userId/groups'>(
+  http.get<{ userId: string }, never, Group[], '/api/v1/users/:userId/groups'>(
     '/api/v1/users/:userId/groups',
-    ({ params }) => {
-      if (params.userId === 'true') {
-        return HttpResponse.json([
-          {
-            id: 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
-            name: 'sampleUser',
-            realName: 'Sam Ple'
-          }
-        ])
-      }
-      return HttpResponse.json([{ id: '', name: 'Alice', realName: '' }])
+    () => {
+      return HttpResponse.json(Array(10).fill(sampleGroup))
     }
   )
 ]
