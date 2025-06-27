471ea999d674674238a0f52d7897d38637997190
Futadaruma <0098yuta@gmail.com>
2025-06-27 09:27:04 +0900

    brushed up sample contest data

--- src/mocks/contests.ts
+++ src/mocks/contests.ts
@@ -1,21 +1,69 @@
 import { http, HttpResponse } from 'msw'
-import { Contest, ContestDetail } from '../lib/apis'
+import { Contest, ContestDetail, ContestTeamDetail } from '../lib/apis'
+import { sampleUsers } from './users'
+
+const sampleContestTeamDetails: ContestTeamDetail[] = [
+  {
+    id: 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
+    name: 'チーム優勝',
+    description: 'これはサンプルのチームです。',
+    result: '優勝',
+    members: sampleUsers,
+    link: 'https://example.com'
+  },
+  {
+    id: 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb',
+    name: 'チーム準優勝',
+    description: 'これはサンプルのチームです。',
+    result: '準優勝',
+    members: sampleUsers,
+    link: 'https://example.com'
+  },
+  {
+    id: 'cccccccc-cccc-cccc-cccc-cccccccccccc',
+    name: 'チーム3位',
+    description: 'これはサンプルのチームです。',
+    result: '3位',
+    members: sampleUsers,
+    link: 'https://example.com'
+  }
+]
+
+const sampleContestDetail: ContestDetail = {
+  id: 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
+  name: 'サンプルコンテスト -sample contest-',
+  description:
+    'これはサンプルのコンテストです。\n←に改行が仕込まれている文字列です。',
+  duration: {
+    since: '2021-01-01T00:00:00Z',
+    until: '2024-01-01T00:00:00Z'
+  },
+  link: '',
+  teams: sampleContestTeamDetails.map(team => {
+    return {
+      id: team.id,
+      name: team.name,
+      result: team.result,
+      members: team.members
+    }
+  })
+}
 
 export const handlers = [
   // apis.getContests
   http.get<never, never, Contest[], '/api/v1/contests'>(
     '/api/v1/contests',
     () => {
-      return HttpResponse.json([
-        {
-          id: 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
-          name: 'sampleContest',
+      return HttpResponse.json(
+        Array(20).fill({
+          id: sampleContestDetail.id,
+          name: sampleContestDetail.name,
           duration: {
-            since: '2021-01-01T00:00:00Z',
-            until: '2021-01-01T00:00:00Z'
+            since: sampleContestDetail.duration.since,
+            until: sampleContestDetail.duration.until
           }
-        }
-      ])
+        })
+      )
     }
   ),
 
@@ -26,17 +74,17 @@ export const handlers = [
     ContestDetail,
     '/api/v1/contests/:contestId'
   >('/api/v1/contests/:contestId', () => {
-    return HttpResponse.json({
-      id: 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
-      name: 'sampleContest',
-      description: 'sampleContest',
-      duration: {
-        since: '2021-01-01T00:00:00Z',
-        until: '2021-01-01T00:00:00Z'
-      },
-      link: '',
-      teams: []
-    })
+    return HttpResponse.json(sampleContestDetail)
+  }),
+
+  // apis.getContestTeam
+  http.get<
+    { contestId: string },
+    never,
+    ContestTeamDetail,
+    '/api/v1/contests/:contestId/teams/:teamId'
+  >('/api/v1/contests/:contestId/teams/:teamId', () => {
+    return HttpResponse.json(sampleContestTeamDetails[0])
   }),
 
   // apis.getUserContests
@@ -45,28 +93,16 @@ export const handlers = [
     never,
     Contest[],
     '/api/v1/users/:userId/contests'
-  >('/api/v1/users/:userId/contests', ({ params }) => {
-    if (params.userId === 'true') {
-      return HttpResponse.json([
-        {
-          id: 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
-          name: 'sampleUser',
-          duration: {
-            since: '2021-01-01T00:00:00Z',
-            until: '2021-01-01T00:00:00Z'
-          }
-        }
-      ])
-    }
-    return HttpResponse.json([
-      {
-        id: 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
-        name: 'sampleUser',
+  >('/api/v1/users/:userId/contests', () => {
+    return HttpResponse.json(
+      Array(20).fill({
+        id: sampleContestDetail.id,
+        name: sampleContestDetail.name,
         duration: {
-          since: '2021-01-01T00:00:00Z',
-          until: '2021-01-01T00:00:00Z'
+          since: sampleContestDetail.duration.since,
+          until: sampleContestDetail.duration.until
         }
-      }
-    ])
+      })
+    )
   })
 ]
