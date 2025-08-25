2d3132c886a3a505bf3c637a18978230046fd91b
Futadaruma <0098yuta@gmail.com>
2025-08-25 15:12:42 +0900

    fix typo

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,13 +1,13 @@
 import { handlers as userHandlers } from './users'
 import { handlers as contestHandlers } from './contests'
-import { handlers as groupeHandlers } from './groups'
+import { handlers as groupHandlers } from './groups'
 import { handlers as projectHandlers } from './projects'
 import { setupWorker } from 'msw/browser'
 
 const handlers = [
   ...userHandlers,
   ...contestHandlers,
-  ...groupeHandlers,
+  ...groupHandlers,
   ...projectHandlers
 ]
 
